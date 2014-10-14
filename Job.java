39401bfdb7b85518d8801919cb1db76333aa0e6a
Matt Burton <matth.burton@hotmail.co.uk>
2014-10-14 12:03:51 +0100

    added doc comments to MyTask

--- Job.java
+++ Job.java
@@ -1,10 +1,18 @@
 public class Job implements Runnable {
-    private int JobNumber;
+    private int jobNumber;
     
+    /**
+     * Single argument constructor for the Job class
+     * 
+     * @param    int jobNumber
+     **/
     Job(int jobNumber) {
         this.jobNumber = jobNumber;
     }
     
+    /**
+     * Method to create a new Run thread
+     */
     public void run() {
         // Undertake required work, here we will emulate it by sleep9ing for a period
         System.out.println("Job: " + jobNumber + " is being processed by thread: " + Thread.currentThread().getName());
